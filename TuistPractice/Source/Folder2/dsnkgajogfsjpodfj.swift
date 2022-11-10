6baae7c542a5c2d736adeda8da57d3a11524b2ed
Seyoung Kim <yy0867@gmail.com>
2022-11-10 14:17:30 +0900

    move appdelegate

---
+++ TuistPractice/Source/Folder2/dsnkgajogfsjpodfj.swift
@@ -0,0 +1,9 @@
+//
+//  dsnkgajogfsjpodfj.swift
+//  TuistPractice
+//
+//  Created by 김세영 on 2022/11/10.
+//  Copyright © 2022 com.Harry.TuistPractice. All rights reserved.
+//
+
+import Foundation
